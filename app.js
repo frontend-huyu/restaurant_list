3550259ef29802c9f0b690cace33b2795513e81d
frontend-huyu <yu.huang0206@gmail.com>
2022-02-28 08:05:05 +0800

    Set homepage layout and index

---
+++ app.js
@@ -0,0 +1,30 @@
+// express setting
+const express = require('express')
+const app = express()
+const port = 3000
+
+// handlebars@3.0.0 setting
+const exphbs = require('express-handlebars')
+// restaurant's list
+const restaurantList = require('./restaurant.json').results
+
+//template engine setting
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// homepage setting
+app.get('/homepage', (req, res) => {
+  res.render('index', { restaurants: restaurantList })
+})
+
+
+
+
+
+
+
+
+// Listen the server when it started
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}/homepage`)
+})
